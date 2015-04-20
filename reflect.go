9dae28234830ed17a50a31e1aff49d1b93ded8b3
tiaotiao <tiaotiaoyly@gmail.com>
2015-04-20 09:37:25 +0800

    add arrays

---
+++ reflect.go
@@ -0,0 +1,32 @@
+package reflectutil
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Assign val to the address of ptr. And the type of val must be *ptr.
+// You can think that it works like this:
+//   v, ok := val.(type(*ptr))
+//   if !ok { panic() }
+//   *ptr = v
+func Assign(ptr interface{}, val interface{}) error {
+	pv := reflect.ValueOf(ptr)
+	if pv.Kind() != reflect.Ptr {
+		panic("ptr must be a pointer")
+	}
+	ev := pv.Elem()
+	if !ev.CanSet() {
+		panic("elem of ptr can not be set")
+	}
+
+	vv := reflect.ValueOf(val)
+
+	if vv.Type() != ev.Type() {
+		return fmt.Errorf("type not match (%v, %v)", vv.Type().String(), ev.Type().String())
+	}
+
+	ev.Set(vv)
+
+	return nil
+}
